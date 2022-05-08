55372e30176b0a758f498d068823e5cc17e8cc3e
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-05-08 05:36:36 -0300

    exercicios 3 e 4 concluídos

--- Front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/first-app/src/App.js
+++ Front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/first-app/src/App.js
@@ -1,33 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-const arrayOfLis = ['Javascript', 'CSS', 'HTML', 'Jest', 'React'];
-
-const OneAndTwo = 'Exercícios 1 e 2 concluídos com sucesso';
-
 const Task = (value) => {
-  const allLis = value.map((li) => <li>{li}</li>);
-  return allLis;
+  return <li key={value}>{value}</li>;
 };
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <ul>{Task(arrayOfLis)}</ul>
-        <p>{OneAndTwo}</p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const tarefas = ['Acordar', 'Tomar café', 'Escovar os dentes', 'Ir trabalhar'];
+
+class App extends React.Component {
+  render() {
+    return <ul>{tarefas.map((tarefa) => Task(tarefa))}</ul>;
+  }
 }
 
 export default App;
