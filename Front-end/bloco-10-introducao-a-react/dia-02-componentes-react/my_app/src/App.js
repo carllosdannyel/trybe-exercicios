6b4e45a12564d18b3dce064e101f38658757672f
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-05-11 15:31:57 -0300

    criando um componente sem muita composicao

--- Front-end/bloco-10-introducao-a-react/dia-02-componentes-react/my_app/src/App.js
+++ Front-end/bloco-10-introducao-a-react/dia-02-componentes-react/my_app/src/App.js
@@ -1,15 +1,61 @@
 import './App.css';
-import Greeting from './Components/Greeting';
-import Image from './Components/Image';
+// import Greeting from './Components/Greeting';
+// import Image from './Components/Image';
+// src/App.js
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Greeting name="Carlos Daniel"/>
-      <hr />
-      <Image source="https://alonza.com.br/wp-content/uploads/2021/07/react-native.png" alternativeText="imagem react"/>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    // Declaração do objeto contendo informações do album01
+    const album01 = {
+      image: 'https://ionicframework.com/docs/icons/logo-react-icon.png',
+      title: 'Mylo Xyloto',
+      releaseDate: {
+        year: '2011',
+        month: '10',
+        day: '24',
+      },
+      others: {
+        recordCompany: 'Capitol, Parlophone',
+        formats: 'CD, digital'
+      }
+    };
+
+    // Declaração do objeto contendo informações do album02
+    const album02 = {
+      image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/5/5d/Coldplay_-_Ghost_Stories.jpg/220px-Coldplay_-_Ghost_Stories.jpg',
+      title: 'Ghost Stories',
+      releaseDate: {
+        year: '2014',
+        month: '05',
+        day: '16',
+      },
+      others: {
+        recordCompany: 'Parlophone',
+        formats: 'CD, digital'
+      }
+    };
+
+    // Retorno do que será renderizado
+    return (
+      <article className='App'>
+        <section>
+          <img src={ album01.image } alt={ album01.title } />
+          <h2>{ album01.title }</h2>
+          <p>Lançamento: { album01.releaseDate.year }</p>
+          <p>Gravadora: { album01.others.recordCompany }</p>
+          <p>Formatos: { album01.others.formats }</p>
+        </section>
+        <section>
+          <img src={ album02.image } alt={ album02.title } />
+          <h2>{ album02.title }</h2>
+          <p>Lançamento: { album02.releaseDate.year }</p>
+          <p>Gravadora: { album02.others.recordCompany }</p>
+          <p>Formatos: { album02.others.formats }</p>
+        </section>
+      </article>
+    );
+  }
 }
 
 export default App;
