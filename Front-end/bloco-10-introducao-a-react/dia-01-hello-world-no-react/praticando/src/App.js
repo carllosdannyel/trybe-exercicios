5f1b3069381052884d0b0af81483e54fa5ab8586
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-05-11 07:11:42 -0300

    chamando uma funcao tick no retorno do meu componente APP

--- Front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/praticando/src/App.js
+++ Front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/praticando/src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import ReactDOM from 'react-dom';
 
 function App() {
-  const textJSX = "Hello, JSX"
-  return (
+
+function tick() {
+  const element = (
     <div className="App">
-      <h1>{textJSX}</h1>
+      <h1>It is {new Date().toLocaleTimeString('pt', { hour12: true })}.</h1>
     </div>
+  );
+  ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
+  // const textJSX = "Hello, JSX"
+  return (
+    <>{tick}</>
   );
 }
 
