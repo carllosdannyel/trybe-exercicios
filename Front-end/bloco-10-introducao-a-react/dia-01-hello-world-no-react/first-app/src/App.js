ad91590bc34e625d36132a9c4b1cd3838a89d23b
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-05-11 08:19:16 -0300

    refatorando o compponent App

--- Front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/first-app/src/App.js
+++ Front-end/bloco-10-introducao-a-react/dia-01-hello-world-no-react/first-app/src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-const Task = (value) => {
-  return <li key={value}>{value}</li>;
-};
-
-const tarefas = ['Acordar', 'Tomar café', 'Escovar os dentes', 'Ir trabalhar'];
-
-class App extends React.Component {
+export default class App extends Component {
   render() {
+
+    const Task = (value) => {
+      return <li key={value}>{value}</li>;
+    };
+
+    const tarefas = [
+      "Acordar",
+      "Tomar café",
+      "Escovar os dentes",
+      "Ir trabalhar",
+    ];
+
     return <ul>{tarefas.map((tarefa) => Task(tarefa))}</ul>;
   }
 }
-
-export default App;
