276371eb1a1a3ca3d443c99853e3998a193e43df
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-05-11 15:24:50 -0300

    aprendendo como utilizar props

--- Front-end/bloco-10-introducao-a-react/dia-02-componentes-react/my_app/src/App.js
+++ Front-end/bloco-10-introducao-a-react/dia-02-componentes-react/my_app/src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import Greeting from './Components/Greeting';
+import Image from './Components/Image';
 
 function App() {
   return (
     <div className="App">
+      <Greeting name="Carlos Daniel"/>
+      <hr />
+      <Image source="https://alonza.com.br/wp-content/uploads/2021/07/react-native.png" alternativeText="imagem react"/>
     </div>
   );
 }
