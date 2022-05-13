ac35ba650091b5e3df5a570b073235670d6a7752
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-05-12 23:31:49 -0300

    criando uma pasta data, e refatorando o componente app

---
+++ Front-end/bloco-10-introducao-a-react/dia-02-componentes-react/my_app/src/Data/Albums.js
@@ -0,0 +1,31 @@
+const albums = [
+  {
+    image: 'https://ionicframework.com/docs/icons/logo-react-icon.png',
+    title: 'Mylo Xyloto',
+    releaseDate: {
+      year: '2011',
+      month: '10',
+      day: '24',
+    },
+    others: {
+      recordCompany: 'Capitol, Parlophone',
+      formats: 'CD, digital'
+    }
+  },
+  // Declaração do objeto contendo informações do album02
+  {
+    image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/5/5d/Coldplay_-_Ghost_Stories.jpg/220px-Coldplay_-_Ghost_Stories.jpg',
+    title: 'Ghost Stories',
+    releaseDate: {
+      year: '2014',
+      month: '05',
+      day: '16',
+    },
+    others: {
+      recordCompany: 'Parlophone',
+      formats: 'CD, digital'
+    }
+  }
+];
+
+export default albums;
