bb879907e0729ea9023330aa83fdd9e8a71d3dfb
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-05-13 09:29:56 -0300

    adicionando comentários ao código

--- Front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/estado-e-eventos/src/App.js
+++ Front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/estado-e-eventos/src/App.js
@@ -2,9 +2,11 @@ import React, { Component } from "react";
 import "./App.css";
 
 export default class App extends Component {
+  // chamando a função constructor para ser executada antes da função render
   constructor() {
     super();
 
+    // startando o estado com alguns valores iniciais
     this.state = {
       botao0: 0,
       botao1: 0,
@@ -12,36 +14,42 @@ export default class App extends Component {
       botao3: 0,
     };
 
+    // dando visibilidade ao objeto this as funções que estão dentro da classe App
     this.handleClick = this.handleClick.bind(this);
     this.botao1 = this.botao1.bind(this);
     this.botao2 = this.botao2.bind(this);
     this.botao3 = this.botao3.bind(this);
   }
 
+  // faz a ligação do estado anterior de uma propriedade do estado criando uma lógica de incrementação
   handleClick() {
     this.setState((estadoAnterior, _props) => ({
       botao0: estadoAnterior.botao0 + 1,
     }));
   }
 
+  // faz a ligação do estado anterior de uma propriedade do estado criando uma lógica de incrementação
   botao1() {
     this.setState((estadoAnterior, _props) => ({
       botao1: estadoAnterior.botao1 + 1,
     }));
   }
 
+  // faz a ligação do estado anterior de uma propriedade do estado criando uma lógica de incrementação
   botao2() {
     this.setState((estadoAnterior, _props) => ({
       botao2: estadoAnterior.botao2 + 1,
     }));
   }
 
+  // faz a ligação do estado anterior de uma propriedade do estado criando uma lógica de incrementação
   botao3() {
     this.setState((estadoAnterior, _props) => ({
       botao3: estadoAnterior.botao3 + 1,
     }));
   }
 
+  // renderiza toda a lógica dentro dela na tela
   render() {
     return (
       <div className='App'>
